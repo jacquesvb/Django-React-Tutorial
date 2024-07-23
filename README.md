1db60c569753ebe80593d62ed1187110341ad668
Jacques Van Blokland <jacquesvb@live.com>
2024-07-22 19:55:24 -0700

    predeploy

---
+++ README.md
@@ -0,0 +1,3 @@
+#A React.js and Django project.
+
+Python environment should be created in the root directory.
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,14 +1,13 @@
-/* eslint-disable react/prop-types */
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import api from "../api";
+import { useNavigate } from "react-router-dom";
 import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
 import "../styles/Form.css";
+import LoadingIndicator from "./LoadingIndicator";
 
 function Form({ route, method }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  // eslint-disable-next-line no-unused-vars
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
@@ -51,6 +50,7 @@ function Form({ route, method }) {
         onChange={(e) => setPassword(e.target.value)}
         placeholder="Password"
       />
+      {loading && <LoadingIndicator />}
       <button className="form-button" type="submit">
         {name}
       </button>
